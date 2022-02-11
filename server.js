d40cb1924a7b1bbcff0085e59344bca5a79a42bd
unknown <ericbwebdev86@gmail.com>
2022-02-11 10:36:34 -0600

    imported path, added notes and wildcard routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //imports
 const express = require('express');
 const { db } = require('./db/db');
+const path = require('path');
 
 //server instatiation
 const app = express();
@@ -16,14 +17,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/api/db', (req, res) => {
     res.send('IF YOU SEE THIS, YOUR SERVER AND ROUTE WORKS!')
     // res.json(db);
 })
 //route for index
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
 
 
 
