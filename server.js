75a268f809e91e7f401aedc01027d4a0cc940238
unknown <ericbwebdev86@gmail.com>
2022-02-11 10:25:11 -0600

    connected index.html added middleware

--- server.js
+++ server.js
@@ -8,11 +8,23 @@ const app = express();
 //PORT variable for local and Heroku use
 const PORT = process.env.PORT || 3000;
 
+//static link to public assets
+app.use(express.static('public'));
+
+//data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //routes
 app.get('/api/db', (req, res) => {
     res.send('IF YOU SEE THIS, YOUR SERVER AND ROUTE WORKS!')
-    res.json(db);
+    // res.json(db);
 })
+//route for index
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
 
 
 //set app to listen at this port
